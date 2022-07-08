31f882f9307fc0a2e499570709515c643a81682e
Andrey Sikorin <amaomi.prog@gmail.com>
2022-07-08 19:58:22 +0300

    Add tests for Square class

---
+++ tests/square-test.cc
@@ -0,0 +1,56 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "clion-misra-cpp2008-6-4-5"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-6-4-1"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-6-3-1"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-8-0-1"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-6-6-5"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-5-0-4"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-11-0-1"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-18-4-1"
+
+#include "square.h"
+
+#include "gtest/gtest.h"
+
+TEST(SquareConstructorTest, constructor) {
+  EXPECT_THROW(chsmv::Square square{""}, std::domain_error) << "Empty string";
+
+  EXPECT_NO_THROW(chsmv::Square square{"a1"}) << "First square on the board";
+  EXPECT_NO_THROW(chsmv::Square square{"e4"}) << "Middle square on the board";
+  EXPECT_NO_THROW(chsmv::Square square{"h8"}) << "Last square on the board";
+
+  EXPECT_THROW(chsmv::Square square{"j1"}, std::domain_error) << "j is invalid file (valid are a-h)";
+  EXPECT_THROW(chsmv::Square square{"a9"}, std::domain_error) << "9 is invalid rank (valid are 1-8)";
+  EXPECT_THROW(chsmv::Square square{"e0"}, std::domain_error) << "0 is invalid rank (valid are 1-8)";
+
+  EXPECT_THROW(chsmv::Square square{"  e4 "}, std::domain_error) << "Extra spaces";
+  EXPECT_THROW(chsmv::Square square{"a1e4"}, std::domain_error) << "Two squares";
+}
+
+class SquareTest : public ::testing::Test {
+ public:
+  SquareTest() : a_1_{"a1"}, d_1_{"d1"}, a_3_{"a3"}, e_4_{"e4"}, h_8_{"h8"} {}
+
+ protected:
+  const chsmv::Square a_1_;
+  const chsmv::Square d_1_;
+  const chsmv::Square a_3_;
+  const chsmv::Square e_4_;
+  const chsmv::Square h_8_;
+};
+
+TEST_F(SquareTest, string_cast) {
+  EXPECT_EQ(static_cast<std::string>(a_1_), "a1");
+  EXPECT_EQ(static_cast<std::string>(d_1_), "d1");
+  EXPECT_EQ(static_cast<std::string>(a_3_), "a3");
+  EXPECT_EQ(static_cast<std::string>(e_4_), "e4");
+  EXPECT_EQ(static_cast<std::string>(h_8_), "h8");
+}
+
+#pragma clang diagnostic pop
