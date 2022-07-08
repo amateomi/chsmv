a7de7b22b7548d6d16cbd19b5fd502f541520465
Andrey Sikorin <amaomi.prog@gmail.com>
2022-07-08 14:10:12 +0300

    Connect GoogleTest and add dummy test

---
+++ tests/test.cc
@@ -0,0 +1,24 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "clion-misra-cpp2008-6-4-5"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-6-4-1"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-6-3-1"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-8-0-1"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-6-6-5"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-5-0-4"
+#pragma ide diagnostic ignored "clion-misra-cpp2008-18-4-1"
+
+#include "chsmv.h"
+#include "gtest/gtest.h"
+
+TEST(Test, test) {
+  ASSERT_EQ(1, 1);
+  chsmv::ChessMoveAnalyzer chess_move_analyzer;
+}
+
+#pragma clang diagnostic pop
