25abb6fc91e6bc9409aca9be41424c6c25bd85be
Andrey Sikorin <amaomi.prog@gmail.com>
2022-08-17 14:21:16 +0300

    Specify MoveDescription enum and add move history

--- include/chsmv.h
+++ include/chsmv.h
@@ -7,21 +7,25 @@
 #ifndef CHSMV_INCLUDE_CHSMV_H_
 #define CHSMV_INCLUDE_CHSMV_H_
 
+#include <list>
 #include <string>
 #include <string_view>
 #include <vector>
 
 namespace chsmv {
 
-enum class MoveStatus { VALID, INVALID };
+enum class MoveDescription { POSSIBLE_MOVE, IMPOSSIBLE_MOVE, PAWN_PROMOTION_NOT_SPECIFIED };
 
 class ChessMoveAnalyzer {
  public:
-  [[nodiscard]] MoveStatus CheckMove(std::string_view board_in_fen, std::string_view move_in_lan) const;
+  [[nodiscard]] MoveDescription CheckMove(std::string_view board_in_fen, std::string_view move_in_lan) const;
 
-  [[nodiscard]] std::string MakeMove(std::string_view board_in_fen, std::string_view move_in_lan) const;
+  [[nodiscard]] std::string MakeMove(std::string_view board_in_fen, std::string_view move_in_lan);
 
   [[nodiscard]] std::vector<bool> GetAvailableMoves(std::string_view board_in_fen, std::string_view square) const;
+
+ private:
+  std::list<std::string> move_history_;
 };
 
 }  // namespace chsmv
