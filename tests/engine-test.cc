43f79dac1b4a9a91ed85135bd1977543e2ae70cb
Andrey Sikorin <amaomi.prog@gmail.com>
2022-08-19 21:31:58 +0300

    Add promotion test

--- tests/engine-test.cc
+++ tests/engine-test.cc
@@ -11,10 +11,12 @@
 
 #include "gtest/gtest.h"
 
+using namespace chsmv;
+
 TEST(LegalMovesTest, KingLegalMove) {
   // White
   {
-    chsmv::Board board{"3N4/1p3K2/1Bp2P2/P2p4/2P5/4RpP1/kn1p1b2/8 w - - 0 1"};
+    Board board{"3N4/1p3K2/1Bp2P2/P2p4/2P5/4RpP1/kn1p1b2/8 w - - 0 1"};
     std::vector<bool> expect{
         // clang-format off
       0, 0, 0, 0, 1, 1, 1, 0,
@@ -27,11 +29,11 @@ TEST(LegalMovesTest, KingLegalMove) {
       0, 0, 0, 0, 0, 0, 0, 0,
         // clang-format on
     };
-    EXPECT_EQ(chsmv::Engine::GetAllLegalMoves(board, chsmv::Square{"f7"}), expect);
+    EXPECT_EQ(Engine::GetAllLegalMoves(board, Square{"f7"}), expect);
   }
   // Black
   {
-    chsmv::Board board{"8/4PqpB/1K5p/2P5/PP3p2/P7/1br2k2/n5N1 b - - 0 1"};
+    Board board{"8/4PqpB/1K5p/2P5/PP3p2/P7/1br2k2/n5N1 b - - 0 1"};
     std::vector<bool> expect{
         // clang-format off
       0, 0, 0, 0, 0, 0, 0, 0,
@@ -44,14 +46,14 @@ TEST(LegalMovesTest, KingLegalMove) {
       0, 0, 0, 0, 1, 1, 1, 0,
         // clang-format on
     };
-    EXPECT_EQ(chsmv::Engine::GetAllLegalMoves(board, chsmv::Square{"f2"}), expect);
+    EXPECT_EQ(Engine::GetAllLegalMoves(board, Square{"f2"}), expect);
   }
 }
 
 TEST(LegalMovesTest, QueenLegalMove) {
   // White
   {
-    chsmv::Board board{"2n3QR/4P2n/1p3p2/1p2p3/P7/pK2b3/P3P3/4k3 w - - 0 1"};
+    Board board{"2n3QR/4P2n/1p3p2/1p2p3/P7/pK2b3/P3P3/4k3 w - - 0 1"};
     std::vector<bool> expect{
         // clang-format off
       0, 0, 1, 1, 1, 1, 0, 0,
@@ -64,11 +66,11 @@ TEST(LegalMovesTest, QueenLegalMove) {
       0, 0, 0, 0, 0, 0, 1, 0,
         // clang-format on
     };
-    EXPECT_EQ(chsmv::Engine::GetAllLegalMoves(board, chsmv::Square{"g8"}), expect);
+    EXPECT_EQ(Engine::GetAllLegalMoves(board, Square{"g8"}), expect);
   }
   // Black
   {
-    chsmv::Board board{"2k5/5b1p/1K6/2r3np/3P3B/N2p2P1/P3q2P/7Q b - - 0 1"};
+    Board board{"2k5/5b1p/1K6/2r3np/3P3B/N2p2P1/P3q2P/7Q b - - 0 1"};
     std::vector<bool> expect{
         // clang-format off
       0, 0, 0, 0, 1, 0, 0, 0,
@@ -81,14 +83,14 @@ TEST(LegalMovesTest, QueenLegalMove) {
       0, 0, 0, 1, 1, 1, 0, 0,
         // clang-format on
     };
-    EXPECT_EQ(chsmv::Engine::GetAllLegalMoves(board, chsmv::Square{"e2"}), expect);
+    EXPECT_EQ(Engine::GetAllLegalMoves(board, Square{"e2"}), expect);
   }
 }
 
 TEST(LegalMovesTest, RookLegalMove) {
   // White
   {
-    chsmv::Board board{"K2n1r2/4p2R/N7/3P2p1/PB6/4nP2/3Pb3/2k4r w - - 0 1"};
+    Board board{"K2n1r2/4p2R/N7/3P2p1/PB6/4nP2/3Pb3/2k4r w - - 0 1"};
     std::vector<bool> expect{
         // clang-format off
       0, 0, 0, 0, 0, 0, 0, 1,
@@ -101,11 +103,11 @@ TEST(LegalMovesTest, RookLegalMove) {
       0, 0, 0, 0, 0, 0, 0, 1,
         // clang-format on
     };
-    EXPECT_EQ(chsmv::Engine::GetAllLegalMoves(board, chsmv::Square{"h7"}), expect);
+    EXPECT_EQ(Engine::GetAllLegalMoves(board, Square{"h7"}), expect);
   }
   // Black
   {
-    chsmv::Board board{"3R4/P3NB2/n3pK2/br1p1n2/P7/7k/1Brp4/8 b - - 0 1"};
+    Board board{"3R4/P3NB2/n3pK2/br1p1n2/P7/7k/1Brp4/8 b - - 0 1"};
     std::vector<bool> expect{
         // clang-format off
       0, 0, 1, 0, 0, 0, 0, 0,
@@ -118,14 +120,14 @@ TEST(LegalMovesTest, RookLegalMove) {
       0, 0, 1, 0, 0, 0, 0, 0,
         // clang-format on
     };
-    EXPECT_EQ(chsmv::Engine::GetAllLegalMoves(board, chsmv::Square{"c2"}), expect);
+    EXPECT_EQ(Engine::GetAllLegalMoves(board, Square{"c2"}), expect);
   }
 }
 
 TEST(LegalMovesTest, BishopLegalMove) {
   // White
   {
-    chsmv::Board board{"8/2P4p/2p5/b3B3/2K1pPbP/4k3/1P2P1pr/n7 w - - 0 1"};
+    Board board{"8/2P4p/2p5/b3B3/2K1pPbP/4k3/1P2P1pr/n7 w - - 0 1"};
     std::vector<bool> expect{
         // clang-format off
       0, 0, 0, 0, 0, 0, 0, 1,
@@ -138,11 +140,11 @@ TEST(LegalMovesTest, BishopLegalMove) {
       0, 0, 0, 0, 0, 0, 0, 0,
         // clang-format on
     };
-    EXPECT_EQ(chsmv::Engine::GetAllLegalMoves(board, chsmv::Square{"e5"}), expect);
+    EXPECT_EQ(Engine::GetAllLegalMoves(board, Square{"e5"}), expect);
   }
   // Black
   {
-    chsmv::Board board{"1N4nN/P6P/1p4P1/2k3pK/2p5/2P1r1P1/2b5/5Q2 b - - 0 1"};
+    Board board{"1N4nN/P6P/1p4P1/2k3pK/2p5/2P1r1P1/2b5/5Q2 b - - 0 1"};
     std::vector<bool> expect{
         // clang-format off
       0, 0, 0, 0, 0, 0, 0, 0,
@@ -155,14 +157,14 @@ TEST(LegalMovesTest, BishopLegalMove) {
       0, 1, 0, 1, 0, 0, 0, 0,
         // clang-format on
     };
-    EXPECT_EQ(chsmv::Engine::GetAllLegalMoves(board, chsmv::Square{"c2"}), expect);
+    EXPECT_EQ(Engine::GetAllLegalMoves(board, Square{"c2"}), expect);
   }
 }
 
 TEST(LegalMovesTest, PawnLegalMove) {
   // White
   {
-    chsmv::Board board{"3r2B1/P7/1P4Rp/1Q6/pp5k/1r5P/2P5/1n2R1K1 w - - 0 1"};
+    Board board{"3r2B1/P7/1P4Rp/1Q6/pp5k/1r5P/2P5/1n2R1K1 w - - 0 1"};
     std::vector<bool> expect{
         // clang-format off
       0, 0, 0, 0, 0, 0, 0, 0,
@@ -175,11 +177,11 @@ TEST(LegalMovesTest, PawnLegalMove) {
       0, 0, 0, 0, 0, 0, 0, 0,
         // clang-format on
     };
-    EXPECT_EQ(chsmv::Engine::GetAllLegalMoves(board, chsmv::Square{"c2"}), expect);
+    EXPECT_EQ(Engine::GetAllLegalMoves(board, Square{"c2"}), expect);
   }
   // Black
   {
-    chsmv::Board board{"3B4/r6p/pK1pP1PP/5bp1/7b/3n4/qPkP4/3r4 b - - 0 1"};
+    Board board{"3B4/r6p/pK1pP1PP/5bp1/7b/3n4/qPkP4/3r4 b - - 0 1"};
     std::vector<bool> expect{
         // clang-format off
       0, 0, 0, 0, 0, 0, 0, 0,
@@ -192,8 +194,15 @@ TEST(LegalMovesTest, PawnLegalMove) {
       0, 0, 0, 0, 0, 0, 0, 0,
         // clang-format on
     };
-    EXPECT_EQ(chsmv::Engine::GetAllLegalMoves(board, chsmv::Square{"h7"}), expect);
+    EXPECT_EQ(Engine::GetAllLegalMoves(board, Square{"h7"}), expect);
   }
+}
+
+TEST(LegalMovesTest, PawnPromotion) {
+  EXPECT_EQ(Engine::IsLegalMove(Board{"Qr5k/K1P1R2B/6p1/p7/p1P4p/P1q2p2/5B2/8 w - - 0 1"}, Move{"c7b8"}),
+            MoveDescription::PAWN_PROMOTION_NOT_SPECIFIED);
+  EXPECT_EQ(Engine::IsLegalMove(Board{"b1B5/3P4/PRN2P2/4Nk2/n4p2/5p2/3K1p1p/r7 b - - 0 1"}, Move{"h2h1"}),
+            MoveDescription::PAWN_PROMOTION_NOT_SPECIFIED);
 }
 
 #pragma clang diagnostic pop
